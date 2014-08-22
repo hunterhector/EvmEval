943edd8dca17c0d68e531c28159a5122d9ce295d
hunterhector <hunterhector@gmail.com>
2014-08-22 18:15:01 -0400

    Add test cases results. Remove systemComparator

--- run/test_score.sh
+++ run/test_score.sh
@@ -1,4 +1,4 @@
 #!/bin/sh 
 system=$1
 echo "Scoring $1 against gold"
-../scorer.py  -g ../data/converted/test.gold.tbf -s ../data/converted/test.$1.tbf -p ../data/tkn/test_tokens/ -d test.$1.comp -o test.$1.eval
+../scorer.py  -g ../data/converted/test.gold.tbf -s ../data/converted/test.$1.tbf -p ../data/tkn/test_tokens/ -d ../sample_score/test.$1.comp -o ../sample_score/test.$1.eval

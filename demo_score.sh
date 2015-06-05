fe7a158fd3b0a8cdf7cd9eafd9fbbdd19ca871cb
hunterhector <hunterhector@gmail.com>
2015-06-05 00:05:46 -0700

    Start making changes towards version 1.5 that do better coreference scoring

--- demo_score.sh
+++ demo_score.sh
@@ -1,8 +1,17 @@
 #!/bin/sh 
 
 echo "Evaluating system A, should be a perfect system\n"
-./scorer_v1.3.py -g data/scoring_demo/gold.tbf -s data/scoring_demo/sample_system_A.tbf -d data/scoring_demo/A_out.tmp -t data/scoring_demo/tkn/ -o data/scoring_demo/perfect_score.tmp -b
-echo "Stored score report at data/scoring_demo/prefect_score.tmp\n"
+./scorer_v1.5.py -g data/scoring_demo/gold.tbf -s data/scoring_demo/sample_system_A.tbf -d data/scoring_demo/A_out.tmp -t data/scoring_demo/tkn/ -o data/scoring_demo/score_prefect.tmp -b
+echo "Stored score report at data/scoring_demo/score_prefect.tmp\n"
+
 echo "Evaluating system B, with one file missed\n"
-./scorer_v1.3.py -g data/scoring_demo/gold.tbf -s data/scoring_demo/sample_system_B.tbf -d data/scoring_demo/B_out.tmp -t data/scoring_demo/tkn/ -o data/scoring_demo/one_miss_score.tmp -b
-echo "Stored score report at data/scoring_demo/one_miss_score.tmp"
+./scorer_v1.5.py -g data/scoring_demo/gold.tbf -s data/scoring_demo/sample_system_B.tbf -d data/scoring_demo/B_out.tmp -t data/scoring_demo/tkn/ -o data/scoring_demo/score_miss_one_file.tmp -b
+echo "Stored score report at data/scoring_demo/score_miss_one_file.tmp"
+
+echo "Evaluating system C, with some token and type errors\n"
+./scorer_v1.5.py -g data/scoring_demo/gold.tbf -s data/scoring_demo/sample_system_C.tbf -d data/scoring_demo/C_out.tmp -t data/scoring_demo/tkn/ -o data/scoring_demo/score_system_C.tmp -b
+echo "Stored score report at data/scoring_demo/score_system_C.tmp"
+
+echo "Evaluating system D, with no response\n"
+./scorer_v1.5.py -g data/scoring_demo/gold.tbf -s data/scoring_demo/sample_system_D.tbf -d data/scoring_demo/D_out.tmp -t data/scoring_demo/tkn/ -o data/scoring_demo/score_empty.tmp -b
+echo "Stored score report at data/scoring_demo/score_empty.tmp"

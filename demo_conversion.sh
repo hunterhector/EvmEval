e54770894472dca6ba674d3e7875e8f829759025
Hector <hunterhector@gmail.com>
2015-07-31 13:37:00 -0400

    Update conversion demo to match the versions

--- demo_conversion.sh
+++ demo_conversion.sh
@@ -11,6 +11,6 @@ output_tbf_basename=data/conversion_demo/gold
 echo "Running XML to Brat Converter..."
 java -jar bin/converter-1.0.0-jar-with-dependencies.jar -i "$ldc_annotation_dir" -o "$brat_output_dir"
 echo "Running tokenizer..."
-java -jar bin/token-file-maker-1.0.3-jar-with-dependencies.jar -a "$brat_output_dir" -t "$brat_output_dir" -e txt -o "$token_table_dir"
+java -jar bin/token-file-maker-1.0.4-jar-with-dependencies.jar -a "$brat_output_dir" -t "$brat_output_dir" -e txt -o "$token_table_dir"
 echo "Converting to TBF format"
 python ./brat2tbf.py -t "$token_table_dir" -d "$brat_output_dir" -o "$output_tbf_basename" -b -w

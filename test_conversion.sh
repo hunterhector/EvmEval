ae5bc1541b8b70d405dfd59e570b72c8566be604
hunterhector <hunterhector@gmail.com>
2015-08-06 10:44:49 -0700

    add to test a way to change extension.

--- test_conversion.sh
+++ test_conversion.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #change this line to the LDC annotation data folder
-ldc_annotation_dir=data/private/LDC2015E68
+ldc_annotation_dir=data/private/LDC2015E73
 
 #change the following lines to your desired output folder
 brat_output_dir=data/private/conversion_test/ann
@@ -9,7 +9,7 @@ token_table_dir=data/private/conversion_test/tkn
 output_tbf_basename=data/private/conversion_test/gold
 
 echo "Running XML to Brat Converter..."
-java -jar bin/converter-1.0.0-jar-with-dependencies.jar -i "$ldc_annotation_dir" -o "$brat_output_dir"
+java -jar bin/converter-1.0.0-jar-with-dependencies.jar -i "$ldc_annotation_dir" -o "$brat_output_dir" -ae ".event_nuggets.xml" -te ".txt"
 echo "Running tokenizer..."
 java -jar bin/token-file-maker-1.0.4-jar-with-dependencies.jar -a "$brat_output_dir" -t "$brat_output_dir" -e txt -o "$token_table_dir"
 echo "Converting to TBF format"

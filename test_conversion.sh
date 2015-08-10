4e59e08e495ab101d2e72ff1a0ad9ad586dfec48
junaraki <junaraki@gmail.com>
2015-08-09 22:26:03 -0400

    Fixed some options for the converter.

--- test_conversion.sh
+++ test_conversion.sh
@@ -10,7 +10,7 @@ token_table_dir=data/private/conversion_test/tkn
 output_tbf_basename=data/private/conversion_test/gold
 
 echo "Running XML to Brat Converter..."
-java -jar bin/converter-1.0.2-jar-with-dependencies.jar -td "$ldc_text_dir" -te "txt" -ad "$ldc_ann_dir" -ae "event_nuggets.xml" -o "$brat_output_dir"
+java -jar bin/converter-1.0.2-jar-with-dependencies.jar -t "$ldc_text_dir" -te "txt" -a "$ldc_ann_dir" -ae "event_nuggets.xml" -o "$brat_output_dir"
 echo "Running tokenizer..."
 java -jar bin/token-file-maker-1.0.4-jar-with-dependencies.jar -a "$brat_output_dir" -t "$brat_output_dir" -e txt -o "$token_table_dir"
 echo "Converting to TBF format"

6744b1df25b7679566084df7ea71ac3ae4c4b14e
hunterhector <hunterhector@gmail.com>
2015-08-24 17:48:06 -0400

    make the system file the last argument in the list

--- demo_validator.sh
+++ demo_validator.sh
@@ -1,6 +1,6 @@
 #!/bin/sh 
 for file in data/test_cases/wrong_format_tests/*.tbf
 do
-	./validator.py -s $file -t data/test_cases/wrong_format_tests/tkn
+	./validator.py -t data/test_cases/wrong_format_tests/tkn -s $file
 	echo "Error code for file "$file" is "$?
 done

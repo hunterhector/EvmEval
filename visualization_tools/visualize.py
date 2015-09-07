f4431bbec8959e7918bba0b4baee3eebb16163b0
hunterhector <hunterhector@gmail.com>
2015-09-07 14:40:35 -0400

    Bug fix to show disconinous mention in the correct way.

--- visualization_tools/visualize.py
+++ visualization_tools/visualize.py
@@ -361,16 +361,11 @@ def token_annotation_2_character_annotation(token_based_annotations, token_map):
     last_id = -1
 
     for sorted_token in sorted(token_based_annotations, key=natural_key):
-        print sorted_token
-        sys.stdin.readline()
-        this_id = ''.join(str(x) for x in natural_key(sorted_token))
+        this_id = int(''.join(str(x) for x in natural_key(sorted_token)))
         if last_id == -1 or this_id != last_id + 1:
             spans.append([])
-            # if last_id != -1:
-            #     print "Adding %s to %s" % (this_id, last_id)
-            #     sys.stdin.readline()
-            # else:
-            #     print "Adding %s" % this_id
+        last_id = this_id
+
         spans[-1].append(sorted_token)
     return [[token_map[s[0]][0], token_map[s[-1]][1] - 1] for s in spans]
 
